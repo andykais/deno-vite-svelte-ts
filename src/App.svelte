b8503c09980afbe9851ac47e0bfb1930d27cea79
Andrew Kaiser <kaisea.rpi@gmail.com>
2024-02-27 18:45:33 -0500

    simple spa router

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,20 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Route, {link} from 'svelte-spa-router'
+  import Home from './routes/index.svelte'
+  import Tags from './routes/tags.svelte'
+
+  const routes = {
+    '/': Home,
+    '/tags': Tags,
+  }
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <a href="/" use:link>home</a>
+    <a href="/tags" use:link>tags</a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Route {routes}/>
 </main>
 
 <style>
